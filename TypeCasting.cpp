c68ff085be9c2120ef3a7c2bd097f68faa247a6a
Yuvraj <takcommunity99@gmail.com>
2025-06-28 17:26:27 +0530

    cpp

--- TypeCasting.cpp
+++ TypeCasting.cpp
@@ -6,6 +6,9 @@ int main() {
     int  value = grade; // Implicit conversion from char to int
     float value2 = 100.99;
     int new_value2 = (int)value2; // Explicit conversion from float to int
+    int divide = value / new_value2; // Implicit conversion from int to float
     cout << value <<endl<< value2<< endl; 
+    cout << divide << endl; // Output the result of the division
+    cout<< (value /(double)value2) << endl; // Implicit conversion from int to double
     return 0;
 }
