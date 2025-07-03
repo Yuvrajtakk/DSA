98ee95c0c5d93dd5e5b7307383610be3ed11e60a
Yuvraj <takcommunity99@gmail.com>
2025-07-03 19:45:24 +0530

    cpp

---
+++ cpp/pyramid_pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, n;
+    cout << "Enter the number of rows: ";
+    cin >> n; // User input for number of rows
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = n-i; j > 0; j--)
+        {
+            cout << " "; // Print the current row number
+        }
+        for (j = 1; j <= i+1; j++)
+        {
+            cout <<j ; // Print the current row number
+        }   
+
+        for (j = i; j > 0; j--)
+        {
+            cout <<j ; // Print the current row number
+        }
+        cout << endl;
+    }
+
+    return 0;   
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -9,22 +9,37 @@ int main()
 
     for (i = 0; i < n; i++)
     {
-        for (j = n-i; j > 0; j--)
+        for(j = 0; j < n - i-1 ; j++)
         {
-            cout << " "; // Print the current row number
+            cout << " "; // Print spaces for right alignment
         }
-        for (j = 1; j <= i+1; j++)
-        {
-            cout <<j ; // Print the current row number
-        }
-        int num = i; // Start from the current row number
-        for (j = i; j > 0; j--)
-        {
-            cout <<j ; // Print the current row number
-            num--;
+        cout << "*"; // Print the first star
+        if(i != 0){
+            for(j = 0; j < 2*i- 1; j++)
+            {
+                cout << " ";
+            }
+            cout << "*"; // Print the second star for rows other than the first
         }
         cout << endl;
     }
 
-    return 0;
+    for (i = 0; i< n-1; i++)
+    {
+        for(j = 0; j <i+1; j++){
+            cout << " "; // Print spaces for left alignment
+        }
+        cout << "*"; // Print the first star
+        if(i != n-2){
+            for(j = 2*(n-i)-5; j> 0; j--)     
+            {
+                cout << " ";
+            }
+            cout << "*"; // Print the second star for rows other than the last
+        }
+        cout << endl;               
+    }
+
+
+    return 0;   
 }
