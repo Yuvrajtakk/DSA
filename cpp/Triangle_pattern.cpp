5004c4b892ef5461424303c484ef74b2e8b8f4e0
Yuvraj <takcommunity99@gmail.com>
2025-07-03 14:54:49 +0530

    cpp

--- cpp/Triangle_pattern.cpp
+++ cpp/Triangle_pattern.cpp
@@ -5,9 +5,32 @@ int main(){
     int i,j,n;
     cout << "Enter the number of rows: ";
     cin >> n; // User input for number of rows
-    for (i =1 ; i <= n; i++){
-        for (j=1; j<= i; j++){
+    for (i = 0 ; i < n; i++){
+        for (j = 0; j<= i; j++){
             cout << j;
+        }
+        cout << endl;
+    }
+    // 0
+    // 01
+    // 012
+    // 0123
+    for (i = 0 ; i < n; i++){
+        int n = i + 1; // Calculate the number of elements in the row
+        for (j = 0; j< i+1; j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+    //0
+    //11
+    //222
+    //3333
+    for (i = 0 ; i < n; i++){
+        char ch = 'A';
+        for (j = 0; j<= i; j++){
+            cout << ch;
+            ch++;
         }
         cout << endl;
     }
