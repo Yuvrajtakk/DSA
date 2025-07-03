81afdf9a04e8d68878984e371defb252c30cbacf
Yuvraj <takcommunity99@gmail.com>
2025-07-03 18:15:45 +0530

    cpp

--- cpp/Inverted_triangle.cpp
+++ cpp/Inverted_triangle.cpp
@@ -7,18 +7,17 @@ int main()
     cout << "Enter the number of rows: ";
     cin >> n; // User input for number of rows
 
-    for (i = 0; i < n; i++){
+    for (i = 0; i < n; i++)
+    {
         char ch = 'A'; // Initialize character for printing letters
         for (j = 0; j < i; j++)
         {
-            cout <<" "; // Print the current row number
-
+            cout << " "; // Print the current row number
         }
-        for(j = 0; j < n-i; j++)
+        for (j = 0; j < n - i; j++)
         {
             cout << ch; // Print the current row number
-            ch++; // Increment character for next letter
-
+            ch++;       // Increment character for next letter
         }
         cout << endl;
     }
--- cpp/reverse_triangle.cpp
+++ cpp/reverse_triangle.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
-    
-int main() {
-        int i,j,n;
+
+int main()
+{
+    int i, j, n;
     cout << "Enter the number of rows: ";
     cin >> n; // User input for number of rows
 
-    for (i = 0 ; i < n; i++){
-        int num = i+1;
-        for (j = 0; j<= i; j++){
+    for (i = 0; i < n; i++)
+    {
+        int num = i + 1;
+        for (j = 0; j <= i; j++)
+        {
             cout << num;
             num--;
         }
         cout << endl;
     }
     // 1
-    // 21       
+    // 21
     // 321
     // 4321
-    for (i = 0 ; i < n; i++){
-        char ch = 'A'+ i;
-        for (j = i+1; j> 0; j--){
+    for (i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (j = i + 1; j > 0; j--)
+        {
             cout << ch;
             ch--;
-        }                                                                                               
+        }
         cout << endl;
     }
 
