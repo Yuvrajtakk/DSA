8a97d4a577017f35e0535939a16d5b48d9d4f638
Yuvraj <takcommunity99@gmail.com>
2026-02-15 13:48:56 +0530

    day1

--- DSA_striver/3_rec-2.cpp
+++ DSA_striver/3_rec-2.cpp
@@ -11,10 +11,21 @@ void print(int i, int n){
     cout << i << endl;
     print(i+1, n);
 }
+void print2(int i, int n){
+    // if (i > n) return;
+    // cout << n-i+1 << endl;
+    // print2(i+1, n);
+    if (i < 1) return;
+    cout << i << endl;
+    print2(i-1,n);
+
+}
 int main(){
     int n;
     cin >> n;
     // recursion(1,n);
-    print(1,n);
+    // print(1,n);
+    print2(n,n);
+
 
 }
